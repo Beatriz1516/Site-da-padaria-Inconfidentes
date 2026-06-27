34be6388cbff59705ee8fa59bcb47bef58e4935e
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-06-27 18:42:40 -0300

    -Adicionada a foto inicial da landpage; -Adicionado a mensagem de boas vindas; -Adicionado um CTA na foto inicial; -Adicionado um timer de loop no carrossel; -Ajuste na logo do site.

--- src/carrossel.js
+++ src/carrossel.js
@@ -1,8 +1,11 @@
+// Código para os botões de navegação do carrossel
+
 const sliderImages = document.querySelectorAll('.slider-image');
 const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
 
 let currentSlide = 0;
+let sliderInterval; // Correção: Declarando a variável do timer
 
 function hideSlider(){
     sliderImages.forEach(item => item.classList.remove('on'))
@@ -11,26 +14,42 @@ function showSlider(){
     sliderImages[currentSlide].classList.add('on')
 }
 
-
 function nextSlider(){
     hideSlider()
-    if(currentSlide === sliderImages.length -1){
+    if(currentSlide === sliderImages.length - 1){
         currentSlide = 0
     } else{
         currentSlide++
     }
     showSlider()
 }
+
 function prevSlider(){
     hideSlider()
     if(currentSlide === 0){
-        currentSlide = sliderImages.length -1
+        currentSlide = sliderImages.length - 1
     } else{
         currentSlide--
     }
     showSlider();
 }
 
+// Código para iniciar o timer do carrossel
+function startSliderTimer() {
+    sliderInterval = setInterval(nextSlider, 5000); 
+}
+// Reseta o timer quando o usuário clica manualmente
+function resetSliderTimer() {
+    clearInterval(sliderInterval);
+    startSliderTimer();
+}
+nextButton.addEventListener('click', () => {
+    nextSlider();
+    resetSliderTimer(); 
+});
+prevButton.addEventListener('click', () => {
+    prevSlider();
+    resetSliderTimer(); 
+});
 
-prevButton.addEventListener('click', prevSlider)
-nextButton.addEventListener('click', nextSlider)
+startSliderTimer();
