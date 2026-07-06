cd8fea16cb85a7164f1a6297195331f09a22bb19
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-07-06 17:52:37 -0300

    -Adicionado novos códigos para a página do pedido.

--- src/itens_produto.js
+++ src/itens_produto.js
@@ -1,58 +1,267 @@
 export const itens = [
     {
+        categoria: "pao-de-queijo",
         imagem: "../imagens/pao_de_queijo_radicional.jfif",
         titulo: "Pão de queijo tradicional - médio",
         preco: "R$ 00,00",
         tempoDePreparo: "x min"
     },
     {
+        categoria: "pao-de-queijo",
         imagem: "../imagens/pão_de_queijo_slider.jfif",
         titulo: "Pão de queijo lanche - grande",
         preco: "R$ 00,00",
         tempoDePreparo: "x min"
     },
     {
+        categoria: "pao-de-queijo",
         imagem: "../imagens/pao_de_queijo_coquitel.jfif",
         titulo: "Pão de queijo coquitel - mini",
         preco: "R$ 00,00",
         tempoDePreparo: "x min"
     },
     {
+        categoria: "pao-de-queijo",
         imagem: "../imagens/pao_de_queijo_de_canastra.png",
         titulo: "Pão de queijo de canastra",
         preco: "R$ 00,00",
         tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/blend_de_tres_queijos.jfif",
+        titulo: "Blend de três queijos",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/Pao_de_queijo_com_requeijao_de_corte.jfif",
+        titulo: "Pão de queijo com requeijão de corte",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/pao_de_queijo_com_carne_desfiada.jfif",
+        titulo: "Pão de queijo com carne seca desfiada",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/pao_de_queijo_romeu_e_julieta.jfif",
+        titulo: "Pão de queijo Romeu e Julieta",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/pao_de_queijo_doce.jfif",
+        titulo: "Pão de queijo com doce de leite viçosa",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "pao-de-queijo",
+        imagem: "../imagens/pao_de_queijo_zero_lactose.jfif",
+        titulo: "Pão de queijo zero lactose",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/cafe_coado_na_hora.jfif",
+        titulo: "Café passado na hora",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/cafe_pingado.jfif",
+        titulo: "Café pingado",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/cafe_com_leite.jfif",
+        titulo: "Café com leite - médio",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/mocha_mineiro.jfif",
+        titulo: "Mocha mineiro",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/cappuccino_dona_sinha.jfif",
+        titulo: "Cappuccino Dona Sinhá",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "cafes",
+        imagem: "../imagens/wafflecinno.jfif",
+        titulo: "Waffleccino",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/chocolate_quente_com_doce.jfif",
+        titulo: "Chocolate quente com doce de leite",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/cha_casca_de_cafe.jfif",
+        titulo: "Chá casca de café",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/chá_de_capim_limao.jfif",
+        titulo: "Chá capim-santo com limão",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/suco_de_milho.jfif",
+        titulo: "Suco de milho verde",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/suco_de_cana.jfif",
+        titulo: "Garapa - suco de cana",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/suco_manga_com_laranja.jfif",
+        titulo: "Vitamina de manga com laranja",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "bebidas",
+        imagem: "../imagens/soda_de_jabuticaba.jfif",
+        titulo: "Soda artesanal de jabuticaba",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "doces",
+        imagem: "../imagens/doce_de_leite_mineiro.jfif",
+        titulo: "Doce de leite mineiro",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "doces",
+        imagem: "../imagens/goiabada_cascão.jfif",
+        titulo: "Goiabada Cascão",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "doces",
+        imagem: "../imagens/doce_de_abobora_com_coco.jfif",
+        titulo: "Doce de abóbora com coco",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "doces",
+        imagem: "../imagens/pe_de_moleque.jfif",
+        titulo: "Pé de moleque",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "doces",
+        imagem: "../imagens/cocada.jfif",
+        titulo: "Cocada",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "quitutes-de-balcao",
+        imagem: "../imagens/torresmo_de_estufa.jfif",
+        titulo: "Torresmo de estufa",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
+    },
+    {
+        categoria: "quitutes-de-balcao",
+        imagem: "../imagens/coxinha_frango_com_catupiry.jfif",
+        titulo: "Coxinha de frango com catupiry",
+        preco: "R$ 00,00",
+        tempoDePreparo: "x min"
     }
-]
+];
 
-const gradeProdutos = document.getElementById("gradeProdutos");
+const mapeamentoCategorias = {
+    "pao-de-queijo": { titulo: "Pão de Queijo", id: "secao-pao-de-queijo" },
+    "cafes": { titulo: "Cafés", id: "secao-cafes" },
+    "bebidas": { titulo: "Bebidas", id: "secao-bebidas" },
+    "doces": {titulo: "Doces", id:"secao-doces"},
+    "quitutes-de-balcao": {titulo: "Quitutes de Balcão", id:"secao-quitutes",
+    "salgados": {titulo: "Salgados", id:"secao-salgados"}
+    }
+};
 
-function renderListaProduto(){
-    itens.forEach((item) => {
-        const imagem = item.imagem;
-        const titulo = item.titulo;
-        const preco = item.preco;
-        const tempoDePreparo = item.tempoDePreparo;
+const cardapioPrincipal = document.getElementById("cardapioPrincipal");
 
-        const card = document.createElement("div");
-        // const img = document.createElement("img");
-        // const p_titulo = document.createElement("p");
-        // const p_preco = document.createElement("p");
+function renderizarCardapioCompleto() {
+    cardapioPrincipal.innerHTML = ""; 
+    Object.keys(mapeamentoCategorias).forEach((chaveCategoria) => {
+        
+        const produtosDaCategoria = itens.filter(item => item.categoria === chaveCategoria);
+        if (produtosDaCategoria.length === 0) return;
 
-        card.innerHTML = `
-        <img src={${imagem}}/>
-        <div class="paoQContent">
-            <p class="NomeProdutoCardapio">{${titulo}}</p>
-            <p class="precoProduto">{${preco}}</p>
-            <div class="complementosCardapio">
-                    <p class="tempoDePreparo">{${tempoDePreparo}}</p>
-                    <button class="direcionaAoProduto"><a href="">Veja melhor</a></button>
-                    <button class="adiciona1AoCarrinho"><a href="">+</a></button>
+        const secaoDivisao = document.createElement("div");
+        secaoDivisao.classList.add("paoDeQueijos");
+
+        const dadosCategoria = mapeamentoCategorias[chaveCategoria];
+        secaoDivisao.innerHTML = `
+            <h2 id="${dadosCategoria.id}" class="tituloDaSecao">${dadosCategoria.titulo}</h2>
+            <div class="grade-produtos"></div>
+        `;
+
+        const gradeProdutos = secaoDivisao.querySelector(".grade-produtos");
+
+        produtosDaCategoria.forEach((item) => {
+            const card = document.createElement("div");
+            card.classList.add("paoQ");
+
+            card.innerHTML = `
+                <img src="${item.imagem}" alt="${item.titulo}">
+                <div class="paoQContent">
+                    <p class="NomeProdutoCardapio">${item.titulo}</p>
+                    <p class="precoProduto">${item.preco}</p>
+                    <div class="complementosCardapio">
+                        <p class="tempoDePreparo">${item.tempoDePreparo}</p>
+                        <button class="direcionaAoProduto"><a href="">Veja melhor</a></button>
+                        <button class="adiciona1AoCarrinho"><a href="">+</a></button>
+                    </div>
                 </div>
-        </div>
-        `
-        gradeProdutos.appendChild(card);
-    })
+            `;
+            gradeProdutos.appendChild(card);
+        });
+
+        cardapioPrincipal.appendChild(secaoDivisao);
+    });
 }
 
-renderListaProduto();
+renderizarCardapioCompleto();
